c0d934246ebf76d49809690127d123dc97ca4f3a
daisaku <daisaku.another31469@gmail.com>
2022-06-06 12:59:24 +0900

    プラグイン flutter(修正）再

--- lib/stogia.dart
+++ lib/stogia.dart
@@ -4,7 +4,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Stogia {
-  static const MethodChannel _channel = MethodChannel('stogie');
+  static const MethodChannel _channel = MethodChannel('stogia');
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
